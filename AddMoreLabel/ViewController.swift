12ec6f48cb0d3ab46a465abd523ef7816cfc53ee
Neo Nguyen <nguyendai0208@gmail.com>
2017-10-02 22:53:56 +0700

    Demo addmore with uilabel.
    demo add more with uilabel

--- AddMoreLabel/ViewController.swift
+++ AddMoreLabel/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak fileprivate var lbText : UILabel!
+    @IBOutlet weak fileprivate var lbMore : UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.lbMore.isHidden = !self.lbText.isTruncated()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +25,22 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+extension UILabel{
+    
+    func isTruncated() ->Bool {
+        guard let labelText = text else {
+            return false
+        }
+        
+        let labelTextSize = (labelText as NSString).boundingRect(
+            with: CGSize(width: frame.size.width, height: .greatestFiniteMagnitude),
+            options: .usesLineFragmentOrigin,
+            attributes: [NSAttributedStringKey.font: font],
+            context: nil).size
+        
+        return labelTextSize.height > bounds.size.height
+    }
 }
 
